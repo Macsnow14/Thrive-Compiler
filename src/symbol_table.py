f1a6ab073d78156b49e65e38f9b5837396062de3
Thrimbda <siyuan.arc@gmail.com>
2017-07-20 15:06:00 +0800

    add symbol table util class

---
+++ src/symbol_table.py
@@ -0,0 +1,12 @@
+"""
+this module contains the util class for symbol table.
+"""
+class SymbolTable(object):
+    """symbol table util"""
+
+    def __init__(self):
+        pass
+
+    def __repr__(self):
+        pass
+    
